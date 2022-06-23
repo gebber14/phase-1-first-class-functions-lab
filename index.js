fff9f944b4d244c47ebe721845fe66e5c61c25c6
Jason Rothgeb <jrothgeb@gmail.com>
2022-06-23 15:49:35 -0500

    commit tree aa2f1327043da3dce3bf0bdeca95e746bb43fe73

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const drivers = ['Antonia','Nuru','Amari',"Mo"];
+//console.log drivers.slice(0,2);
+const returnFirstTwoDrivers = function(drivers) {
+    return (drivers.slice(0,2)); 
+}
+const returnLastTwoDrivers = function(drivers) {
+    return (drivers.slice(2)); 
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+}   
+//createFareMultiplier(function(multiplier, fare) {return multiplier * fare}); 
+
+//const fareMult = (multiplier,fare) => multiplier * fare;
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(drivers,chooseDriver) {
+    return chooseDriver(drivers);
+}
